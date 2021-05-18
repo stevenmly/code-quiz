6795b341c11b490cb0fc8d1c5e448ddf6e18311b
Steven Ly <ly.steven16@gmail.com>
2021-05-18 14:57:07 -0500

    Created a timer function and a startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,34 @@
+var timerEl = document.querySelector("#timer")
+var startButtonEL = document.querySelector("#start-btn")
+
 // global variables
-var timer = 0
-var score = 0
-var questionNum = 0
+var timer = 0;
+var score = 0;
+var questionNum = 0;
+var currentTime;
 
 // function to start quiz
-function startQuiz () {
-    
+function startQuiz() {
+    timer = 60
+    startTimer();
+
+
+
 }
+
+function startTimer() {
+    currentTime = setInterval(function() {
+        if (timer > 0) {
+            timerEl.textContent = timer;
+            timer--;
+        }
+        else {
+            clearInterval(currentTime);
+            timerEl.textContent = 0
+        }
+    }, 1000);
+}
+
+startQuiz();
+
+startButtonEL.addEventListener("click", startQuiz);
