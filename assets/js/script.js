6293ef5828f907b31137800805c8ba1b61630183
Steven Ly <ly.steven16@gmail.com>
2021-05-20 14:42:50 -0500

    Created Javascript code questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+questionObjs = [
+    {
+        question: "What is the correct way to implement comments in JavaScript?",
+        choices: ["&&", "//", "<!-- -->", "[]"],
+        answer: "//"
+    },
+    {
+        question: "Which of these variables correctly represents camel casing?",
+        choices: ["var = TimeLeft","var = timeleft","var = timeLeft","var = Timeleft"],
+        answer: "var = timeLeft"
+    },
+    {
+        question: "Which one of these is NOT a  primitive data type in JavaScript?",
+        choices: ["Number", "String", "Object", "Boolean"],
+        answer: "Object"
+    },
+    {
+        question: "Examine the following array. var names = ['steven', 'Logan', 'Lindsay', 'Taylor']. How would you select the name 'Lindsay' from the array?",
+        choices: ["names[0]", "names[1]", "names[2]", "names[3]"],
+        answer: "names[2]"
+    },
+    {
+        question: "",
+        choices: [],
+        answer: 
+    },
+]
+
 var timerEl = document.querySelector("#timer")
 var startButtonEL = document.querySelector("#start-btn")
 
@@ -10,10 +38,9 @@ var currentTime;
 // function to start quiz
 function startQuiz() {
     timer = 60
+    score = 0;
     startTimer();
-
-
-
+    
 }
 
 function startTimer() {
@@ -31,4 +58,4 @@ function startTimer() {
 
 startQuiz();
 
-startButtonEL.addEventListener("click", startQuiz);
+
